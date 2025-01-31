7a4bfbe302224dd59c0c7fe50294f1a0ccb33e55
Theo <sparvish1337@gmail.com>
2025-01-31 23:43:35 +0100

    wiped

--- functions/mysql.js
+++
@@ -1,9 +0,0 @@
-var mysql = require("mysql2");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "vpsf",
-});
-
-module.exports = { connection };
